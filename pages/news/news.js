8ee9ac5535be03d70fa74b50e7b7225988c29bde
NX622848 <2412739949@qq.com>
2023-07-13 17:50:33 +0800

    init

--- pages/news/news.js
+++ pages/news/news.js
@@ -1,66 +1,44 @@
-// pages/news/news.js
+import TIM from 'tim-wx-sdk';
+import TIMUploadPlugin from 'tim-upload-plugin';
+import TIMProfanityFilterPlugin from 'tim-profanity-filter-plugin';
+import { genTestUserSig }  from '../../TUIKit/debug/GenerateTestUserSig';
+
 Page({
+    data: {
+        config: {
+            userID: 'nx622848', //User ID
+            SDKAPPID: 1400822363, // Your SDKAppID
+            SECRETKEY: '4296cb18eb660f3026386e05808e9bc1f119e8670638ea2067a12d6ed7c29685', // Your secretKey
+            EXPIRETIME: 604800,
+        }
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
-})
+    onLoad() {
+        const userSig = genTestUserSig(this.data.config).userSig 
+        wx.$TUIKit = TIM.create({
+            SDKAppID: this.data.config.SDKAPPID
+        })
+        wx.$chat_SDKAppID = this.data.config.SDKAPPID;
+        wx.$chat_userID = this.data.config.userID;
+        wx.$chat_userSig = userSig;
+        wx.$TUIKitTIM = TIM;
+        wx.$TUIKit.registerPlugin({ 'tim-upload-plugin': TIMUploadPlugin });
+        wx.$TUIKit.registerPlugin({ 'tim-profanity-filter-plugin': TIMProfanityFilterPlugin });
+        wx.$TUIKit.login({
+            userID: this.data.config.userID,
+            userSig
+        });
+        wx.setStorage({
+            key: 'currentUserID',
+            data: [],
+        });
+        wx.$TUIKit.on(wx.$TUIKitTIM.EVENT.SDK_READY, this.onSDKReady,this);
+    },
+    onUnload() {
+        wx.$TUIKit.off(wx.$TUIKitTIM.EVENT.SDK_READY, this.onSDKReady,this);
+    },
+    onSDKReady() {
+        const TUIKit = this.selectComponent('#TUIKit');
+        TUIKit.init();
+    }
+});
