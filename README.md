006fa623228e5687ec936628ede3221e22728feb
NX622848 <2412739949@qq.com>
2023-07-20 16:59:33 +0800

    base layout

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # loveSpace
 情侣记录app
+## 标签
+```
+block 只接受控制元素 不会渲染到页面
+
+<!-- 控制显示隐藏 -->
+<!-- if 不渲染 -->
+<block wx:if="{{false}}">
+  11
+</block>
+
+<text hidden="{{true}}">
+22
+</text>
+```
+## 双向绑定  用 model:value
+```
+<input model:value="{{name}}" type="text"/>
+```
+
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,19 +4,118 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userlist:{
-      username:'喜洋洋',
-      userImage: '../../assets/images/avatar1.webp',
-      LoveTime:'',
-      endTime: 0,
+    name:'sdf',
+    LoveTime:'',
+    endTime: 0,
+    userlist:[
+      {
+        id:1,
+        username:'喜洋洋',
+        userImage: '/assets/images/avatar1.jpg',
+      },
+      {
+        id:2,
+        username:'美洋洋',
+        userImage: '/assets/images/avatar2.jpg',
+      },
+    ],
+    photoList:[
+      '/assets/images/avatar1.jpg',
+      '/assets/images/avatar2.jpg',
+      '/assets/images/avatar1.jpg',
+      '/assets/images/avatar2.jpg',
+      '/assets/images/avatar1.jpg',
+      '/assets/images/avatar2.jpg',
+      '/assets/images/avatar1.jpg',
+      '/assets/images/avatar2.jpg',
+      '/assets/images/avatar1.jpg',
+      '/assets/images/avatar2.jpg',
+    ],
+    plans:{
+      plan:[
+        {
+          id:1,
+          planTime: "21/12",
+          planendTime: "21/32",
+          plancontent: "下周去吃火锅",
+        },
+        {
+          id:2,
+          planTime: "21/12",
+          planendTime: "21/32",
+          plancontent: "赚钱",
+        },
+        {
+          id:3,
+          planTime: "21/12",
+          planendTime: "21/32",
+          plancontent: "下周去吃火锅",
+        },
+        {
+          id:4,
+          planTime: "21/12",
+          planendTime: "21/32",
+          plancontent: "赚钱",
+        },
+        {
+          id:5,
+          planTime: "21/12",
+          planendTime: "21/32",
+          plancontent: "下周去吃火锅",
+        },
+      ]
+    },
+    leaves:{
+      leave:[
+        {
+          id:1,
+          leaveTime: "33/21",
+          leaveEndTime: "32/21",
+          leavecontent: "留言1"
+        },
+        {
+          id:2,
+          leaveTime: "33/21",
+          leaveEndTime: "32/21",
+          leavecontent: "留言2"
+        },
+        {
+          id:3,
+          leaveTime: "33/21",
+          leaveEndTime: "32/21",
+          leavecontent: "留言1"
+        },
+        {
+          id:4,
+          leaveTime: "33/21",
+          leaveEndTime: "32/21",
+          leavecontent: "留言2"
+        },
+        {
+          id:5,
+          leaveTime: "33/21",
+          leaveEndTime: "32/21",
+          leavecontent: "留言1"
+        },
+      ]
     }
   },
-
+  clickleave(e){
+    console.log("点击+1",e.target.dataset);
+  },
+  uploadFile(){
+    wx.chooseMedia({
+      count: 1,
+      success(res){
+        console.log(res);
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
